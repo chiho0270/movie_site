0bd2ea6e7c6ea620aaea6826b9a7ea520dd28526
juahn Shin <juahn112@gmail.com>
2025-05-15 14:26:28 +0900

    폴더명 수정정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/Mainpage";
+import MainPage from "./commons/Main";
 
 function App() {
   return(
