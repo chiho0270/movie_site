04e9a4fbee0df45d3ee681127670b7b82e539352
hse <sseejm13@naver.com>
2025-05-31 18:22:07 +0900

    박스오피스

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const authRouter = require('./routes/auth');
+const boxofficeRouter = require('./routes/boxoffice');
 
 app.use(express.json());
 app.use('/api', authRouter);
+app.use('/api/search', boxofficeRouter);
+
 
 db.sequelize.sync().then(() => {
   app.listen(3000, () => {
