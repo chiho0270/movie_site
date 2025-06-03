692ec5e87f448c8d042071946b1014a2eba02de2
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2025-06-04 02:57:01 +0900

    통합 시작
    02:56 시작

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '0000',
+    database: process.env.DB_DATABASE || 'database_name',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql'
   }
 };
